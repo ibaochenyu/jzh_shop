7e25ee378fc83d89e7e13f99bb026e087723da5b
yesanggl <yesanggl1@gmail.com>
2024-05-20 23:09:01 +0800

    第一次提交

---
+++ src/main/java/cn/ibaochenyu/jzh_shop/JzhShopApplication.java
@@ -0,0 +1,13 @@
+package cn.ibaochenyu.jzh_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzhShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JzhShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ibaochenyu/jzh_shop/JzhShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ibaochenyu.jzh_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JzhShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
