f10cddcd0ea7e1e4c3132eba4cad39cf3963952d
yesanggl <yesanggl1@gmail.com>
2024-06-06 17:17:52 +0800

    利用dozer进行对象转换

--- src/test/java/cn/ibaochenyu/jzh_shop/JzhShopApplicationTests.java
+++ src/test/java/cn/ibaochenyu/jzh_shop/JzhShopApplicationTests.java
@@ -1,13 +1,35 @@
 package cn.ibaochenyu.jzh_shop;
 
+import cn.ibaochenyu.jzh_shop.dao.entity.BasicDO;
+import cn.ibaochenyu.jzh_shop.dto.resp.BasicQueryRespDTO;
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+import com.github.dozermapper.core.Mapper;
 @SpringBootTest
 class JzhShopApplicationTests {
 
+    protected static Mapper BEAN_MAPPER_BUILDER;//import com.github.dozermapper.core.Mapper;
+
+    static {
+        BEAN_MAPPER_BUILDER = DozerBeanMapperBuilder.buildDefault();
+    }
     @Test
-    void contextLoads() {
+    void 利用dozer进行对象转换() {
+        BasicDO source = new BasicDO();
+        Class<cn.ibaochenyu.jzh_shop.dto.resp.BasicQueryRespDTO> clazz = BasicQueryRespDTO.class;
+        source.setId(1);
+        source.setWorkId(3);
+        source.setName("黄小鑫");
+        source.setSex(1);
+        source.setHomeAddress("黄小鑫的家");
+        BasicQueryRespDTO temp=Optional.ofNullable(source)
+                .map(each -> BEAN_MAPPER_BUILDER.map(each, clazz))
+                .orElse(null);
+        int a=2;
+
     }
 
 }
